853dcb1caa4ce92151d07729838d395cbfc566f8
Ryeo <32667987+y2h1987@users.noreply.github.com>
2025-06-02 19:06:23 +0800

    xdwad

--- lesson6/root.js
+++ lesson6/root.js
@@ -2,6 +2,8 @@ function WelcomeMessagesTitle() {
   return <p>Welcome Messages</p>
 }
 
+function WelcomeMessageTom() {
+  return <li>Welcome Tom from Singapore</li>
 }
 
 function WelcomeMessageJerry() {
@@ -20,7 +22,7 @@ function WelcomeMessagesList() {
   </ul>
 }
 
-
+function WelcomeMessagesSection() {
   return <div>
     <WelcomeMessagesTitle />
     <WelcomeMessagesList />
